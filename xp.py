7e1f9065dbb171718923dcce834a5196f8a9eec2
Paula Mendez <dev.paulamendez@gmail.com>
2022-07-20 14:45:23 -0700

    add experience

---
+++ xp.py
@@ -0,0 +1,79 @@
+experience = [
+    {
+        "title": "DevOps Engineer",
+        "company": "Covetrus",
+        "description": [
+            "Improve and further automate the CD pipeline using Terraform and Harness to deploy containerized apps hosted on AWS.",
+            "Serve as central point of contact of all Infrastructure-related topics for specific app-dev teams.",
+            "Participate and collaborate in the continuous improvement of CI/CD pipelines to ensure optimal site reliability."
+        ],
+        "dates": "Sep 2021 – Present",
+        "skills":["aws", "terraform", "docker", "mongo", "harness", "jenkins", "azure"]
+    },
+    {
+        "title": "DevOps Engineer",
+        "company": "Capital One",
+        "description": [
+            "Worked as an Embedded DevOps resource, managing several Kubernetes micro-services hosted on Google Cloud Platform.",
+            "Maintained fully automated CI/CD pipelines for code deployment using Jenkins and Groovy.",
+            "Participated in weekly Scrum meetings, and communicated daily with developers and other team members to ensure maximum productivity and efficiency."
+        ],
+        "dates": "Feb 2021 – Sep 2021",
+        "skills":["gcp", "aws", "terraform", "docker", "mongo", "jenkins"]
+    },
+    {
+        "title": "Cloud Engineer",
+        "company": "Covetrus",
+        "description": [
+            "Work as part of a cross-functional group to provide customized platform solutions to fit their needs.",
+            "Provided technical guidance regarding architecture, coding and performance to help boost productivity and results.",
+            "Automated cloud daily tasks, including database IAM using AWS CLI, Python, Bash/ZSH.",
+        ],
+        "dates": "Feb 2021 – Sep 2021",
+        "skills":["python", "aws", "mongo", "sql", "harness", "jenkins"]
+    },
+    {
+        "title": "Systems Administrator",
+        "company": "Covetrus",
+        "description": [
+            "Acted as second and third level support through user-generated service Jira tickets, diagnosing root causes and providing long-term solutions.",
+            "Deployed automated MDM processes using jamf to a previously unmanaged macOS fleet, deploying policies, workflows and patching.",
+            "Wrote, tested and deployed PowerShell Scripts and Group Policies to automate and maintain the Windows Infrastructure.",
+        ],
+        "dates": "Nov 2018 – Oct 2020",
+        "skills":["python", "powershell", "jamf", "sql", "active-directory", "c-sharp"]
+    },
+    {
+        "title": "IT Support Specialist",
+        "company": "Covetrus",
+        "description": [
+            "Managed Microsoft Active Directory and hardware asset management, imaging and deployment of workstations.",
+            "Provided technical support for 1000+ employees in 6 offices located all over the United States.",
+            "Performed diagnostic and troubleshooting of multiple system issues, documented issues and SOPs.",
+        ],
+        "dates": "Nov 2018 – Oct 2020",
+        "skills":["python", "powershell", "jamf", "active-directory",  "c-sharp"]
+    },
+    {
+        "title": "Research Programmer",
+        "company": "Laboratory for Knowledge Discovery in Databases",
+        "description": [
+            "Provided support for data mining and machine learning on a text extraction project partnered with Lawrence Livermore National Lab.",
+            "Implemented a NoSQL Database (MongoDB) to store machine learning results and display on the Flask web app.",
+            "Managed the front-end user interface for customer easy access using Python (Flask), JavaScript, HTML and CSS.",
+        ],
+        "dates": "Aug 2016 – May 2018",
+        "skills":["python", "flask", "javascript", "html", "css", "mongo"]
+    },
+    {
+        "title": "Developer",
+        "company": "Beef Cattle Institute, Kansas State University",
+        "description": [
+            "Developed an event monitoring and classification system in Python for bovine infectious diseases using current Internet of Things (IoT) technology.",
+            "Implemented a Python support for machine learning component for disease classification based on spatiotemporal pattern analysis from timestamped wearable sensor logs.",
+            "Wrote and optimized SQL queries to retrieve and use data accordingly.",
+        ],
+        "dates": "April 2016 – May 2016",
+        "skills":["python", "sql"]
+    },
+]
